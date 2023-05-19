c64d3c46b7e67781d29a87968de85f5379be7048
TimidBagel <slaprabbit805@gmail.com>
2023-05-19 14:45:34 -0400

    Copied project files

---
+++ src/main.rs
@@ -0,0 +1,186 @@
+use serde::{Deserialize, Serialize};
+use std::{
+    fs::File,
+    io::{stdin, BufReader, Result, Write},
+};
+
+// Represents a task with its name, description, due date, and completion status.
+#[derive(Serialize, Deserialize)]
+struct Task {
+    name: String,
+    desc: String,
+    due_date: String,
+    done: bool,
+}
+
+// Main program procedure
+fn main() {
+    // Initializes vector of tasks, and copies data from `tasks.json` if file exists.
+    let mut tasks: Vec<Task> = match read_tasks() {
+        Ok(tasks) => tasks,
+        Err(_) => {
+            vec![]
+        }
+    };
+
+    // Runtime loop
+    loop {
+        println!(
+            "\nWould you like to? (eg: '1')\n1. View tasks\n2. Add a task\n3. Complete task\n4. Delete task"
+        );
+        let resp = read_line();
+
+        match resp.as_str() {
+            "1" => view_tasks(&mut tasks),
+
+            "2" => {
+                let new_task = create_task();
+                add_task(&mut tasks, new_task);
+            }
+
+            "3" => {
+                view_tasks(&tasks);
+
+                println!("\nSelect a task to mark as complete:");
+
+                if let Some(index) = read_index_input(&tasks) {
+                    complete_task(&mut tasks, index);
+                } else {
+                    continue;
+                }
+            }
+
+            "4" => {
+                view_tasks(&tasks);
+
+                println!("\nSelect a task to delete:");
+
+                if let Some(index) = read_index_input(&tasks) {
+                    delete_task(&mut tasks, index);
+                } else {
+                    continue;
+                }
+            }
+
+            // If other input, save task vector to `tasks.json` and exit program.
+            _ => {
+                println!("Serializing data...");
+                let serialized_tasks = serde_json::to_string(&tasks).expect("Serialization failed");
+                println!("Data serialized");
+
+                println!("Creating file...");
+                let mut file = File::create("tasks.json").expect("File creation failed");
+                println!("File created");
+
+                println!("Saving work...");
+                file.write_all(serialized_tasks.as_bytes())
+                    .expect("Writing to file failed");
+                println!("Work saved");
+
+                println!("Exiting successfully");
+                break;
+            }
+        }
+    }
+}
+
+// Reads tasks from the "tasks.json" file and returns them as a vector.
+fn read_tasks() -> Result<Vec<Task>> {
+    let file = File::open("tasks.json")?;
+    let reader = BufReader::new(file);
+    let tasks: Vec<Task> = serde_json::from_reader(reader)?;
+    Ok(tasks)
+}
+
+// Adds a new task to the vector of tasks.
+fn add_task(tasks: &mut Vec<Task>, new_task: Task) {
+    tasks.push(new_task);
+}
+
+// Creates a new task by prompting the user for its name, description, and due date.
+fn create_task() -> Task {
+    println!("Enter a name for 'new_task':");
+    let name: String = read_line();
+
+    println!("Enter a short description for '{name}':");
+    let desc: String = read_line();
+
+    println!("Enter a due date for '{name}':");
+    let due_date: String = read_line();
+
+    let done: bool = false;
+
+    Task {
+        name,
+        desc,
+        due_date,
+        done,
+    }
+}
+// Reads user input for the task index and returns it as an `Option<usize>`.
+// If the input is invalid or out of range, it returns `None`.
+fn read_index_input(tasks: &[Task]) -> Option<usize> {
+    let index: usize = match read_line().parse::<usize>() {
+        Ok(num) => num - 1,
+        Err(_) => {
+            println!("Input must be a valid index!");
+            return None;
+        }
+    };
+
+    if index >= tasks.len() {
+        println!("Invalid task index!");
+        return None;
+    } else {
+        return Some(index);
+    }
+}
+
+// Reads a line of input from the user.
+fn read_line() -> String {
+    let mut input: String = String::new();
+    match stdin().read_line(&mut input) {
+        Ok(_) => {
+            input = input.trim().to_string();
+        }
+        Err(error) => {
+            eprintln!("Error: {}", error);
+        }
+    }
+
+    input
+}
+
+// Displays the list of tasks to the user.
+fn view_tasks(tasks: &Vec<Task>) {
+    println!(); // newline
+
+    for (i, task) in tasks.iter().enumerate() {
+        println!(
+            "\t{}. {} : {} : Done - {}\n\t{}\n",
+            i + 1,
+            task.name,
+            task.due_date,
+            task.done,
+            task.desc
+        );
+    }
+}
+
+// Marks a task as complete at the specified index.
+fn complete_task(tasks: &mut Vec<Task>, index: usize) {
+    if let Some(task) = tasks.get_mut(index) {
+        task.done = true;
+    } else {
+        println!("Invalid task index! (im gay)");
+    }
+}
+
+// Deletes a task at the specified index.
+fn delete_task(tasks: &mut Vec<Task>, index: usize) {
+    if let Some(_) = tasks.get(index) {
+        tasks.remove(index);
+    } else {
+        println!("Invalid task index! >:(");
+    }
+}
